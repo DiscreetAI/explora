483c2855106e3889a64ed2301cdbbeafcbd74fe7
neeleshdodda44 <ndodda@berkeley.edu>
2019-02-02 01:18:03 -0800

    add client, looping frontend

--- core/status_server_client.py
+++ core/status_server_client.py
@@ -1,9 +1,21 @@
 import requests
+import json
+import logging
 
+
+logging.basicConfig(level=logging.DEBUG,
+    format='[StatusServerClient] %(message)s')
 
 class StatusServerClient(object):
     """
     """
     def __init__(self, config):
         self.server_get_url = config['SERVER']['server_get_url']
+
+    def get_latest_stats(self, job_uuid):
+        response = requests.get(self.server_get_url.format(job_uuid=job_uuid))
+        response_dict = json.loads(response.text)
+        assert 'status' in response_dict and response_dict['status'] == 'success', \
+            "GET failed!"
+        return response_dict['job_dict']
         
